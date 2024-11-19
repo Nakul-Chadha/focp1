68faa69e96a8d6ae0c85c02676730c7ea37efb63
EnderDefender-Nakul_Chadha <183076649+Nakul-Chadha@users.noreply.github.com>
2024-11-19 11:22:20 +0530

    Commit Done

---
+++ Assignment/Assignment_1.4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int no1,no2;
+    printf("Enter two no. to be swaped : ");
+    scanf("%d",&no1);
+    scanf("%d",&no2);
+    printf("Two no. entered by the user are : %d and %d",no1,no2);
+    
+}
