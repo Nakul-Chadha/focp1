12da4888ae333746cf5e67c620d16f5db9718dd3
EnderDefender-Nakul_Chadha <183076649+Nakul-Chadha@users.noreply.github.com>
2024-11-20 11:22:54 +0530

    Commit Done

--- Assignment/Assignment_1.4.c
+++ Assignment/Assignment_1.4.c
@@ -1,4 +1,40 @@
 #include<stdio.h>
+void temp_swap(int a, int b)
+{
+    printf("Swaping by temporary varriables : ");
+    printf("\nBefore swaping : no1=%d no2=%d",a,b);
+    int c=a;
+    a=b;
+    b=c;
+    printf("\nAfter swaping : no1=%d no2=%d",a,b);
+}
+void add_swap(int a, int b)
+{
+    printf("\nSwaping by adding varriables : ");
+    printf("\nBefore swaping : no1=%d no2=%d",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("\nAfter swaping : no1=%d no2=%d",a,b);
+}
+void bitwise_swap(int a, int b)
+{
+    printf("\nSwaping by bitwise : ");
+    printf("\nBefore swaping : no1=%d no2=%d",a,b);
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    printf("\nAftter swaping : no1=%d no2=%d",a,b);
+}
+void multiply_swap(int a, int b)
+{
+    printf("\nSwaping by multiplication and division : ");
+    printf("\nBefore swaping : no1=%d no2=%d",a,b);
+    a=a*b;
+    b=a/b;
+    a=a/b;
+    printf("\nAfter swaping : no1=%d no2=%d",a,b);
+}
 int main()
 {
     int no1,no2;
@@ -6,5 +42,5 @@ int main()
     scanf("%d",&no1);
     scanf("%d",&no2);
     printf("Two no. entered by the user are : %d and %d",no1,no2);
-    
+    bitwise_swap(no1,no2);
 }
