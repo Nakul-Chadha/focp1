4e3c5296da3644c43e35339452eb9d1318e90fc7
EnderDefender-Nakul_Chadha <183076649+Nakul-Chadha@users.noreply.github.com>
2024-11-24 21:39:30 +0530

    Commit Done

---
+++ Assignment/Assignment_1.10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void Pascal(int n)
+{
+    for (int l= 1; l<= n; l++) {
+        for (int s= 1; s<= n - l; s++)
+            printf("  ");
+
+        int coeffi = 1;
+        for (int i = 1; i <= l; i++) {
+
+            printf("%4d", coeffi);
+            coeffi = coeffi * (l - i) / i;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the no. of lines : ");
+    scanf("%d",&n);
+    Pascal(n);
+    return 0;
+}
