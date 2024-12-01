9e61dd0653237c3c8bff0cb932765cbc6178bc88
EnderDefender-Nakul_Chadha <183076649+Nakul-Chadha@users.noreply.github.com>
2024-12-01 11:41:32 +0530

    Commit Done

---
+++ Assignment/Assignment_2.5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the no. of students' marks : ");
+    scanf("%d",&a);
+    
+    int Marks[a];
+    for(int i=0;i<a;i++)
+    {
+        int b;
+        printf("Enter marks :");
+        scanf("%d",&b);
+        Marks[i]=b;
+    }
+
+    for(int i=0;i<a;i++)
+    {
+        printf("Student marks : %d\n",Marks[i]);
+    }
+    int sum=0;
+    for(int i=0;i<a;i++)
+    {
+        sum+=Marks[i];
+    }
+
+    printf("Total sum of marks scored by students in array = %d",sum);
+}
