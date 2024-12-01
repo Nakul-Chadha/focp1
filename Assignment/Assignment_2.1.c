6f91d6d3cf06a080be39c26170df8c66dba96aa7
EnderDefender-Nakul_Chadha <183076649+Nakul-Chadha@users.noreply.github.com>
2024-12-01 10:20:04 +0530

    Commit Done

---
+++ Assignment/Assignment_2.1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the no. of students' marks : ");
+    scanf("%d",&a);
+    
+    int arr[a];
+    for(int i=0;i<a;i++)
+    {
+        int b;
+        printf("Enter marks :");
+        scanf("%d",&b);
+        arr[i]=b;
+    }
+
+    for(int i=0;i<a;i++)
+    {
+        printf("Student marks : %d\n",arr[i]);
+    }
+
+    for(int i =0;i<a;i++)
+    {
+        arr[i]+=5;
+    }
+
+    printf("Students' marks after increment of 5 : \n");
+
+    for(int i=0;i<a;i++)
+    {
+        printf("Student marks : %d\n",arr[i]);
+    }
+}
