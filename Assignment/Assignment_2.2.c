2e71b4770100b89d8a1554f25c09d8ef4049c2d3
EnderDefender-Nakul_Chadha <183076649+Nakul-Chadha@users.noreply.github.com>
2024-12-01 11:25:32 +0530

    Commit Done

---
+++ Assignment/Assignment_2.2.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the no. of students' marks : ");
+    scanf("%d",&a);
+    
+    int arr[a];
+    for(int i=0;i<a;i++)
+    {
+        int b;
+        printf("Enter marks :");
+        scanf("%d",&b);
+        arr[i]=b;
+    }
+
+    for(int i=0;i<a;i++)
+    {
+        printf("Student marks : %d\n",arr[i]);
+    }
+
+    for(int i=0;i<a;i++)
+    {
+        if(arr[i]>=75)
+        {
+            printf("\nGrade A");
+        }
+
+        else if(arr[i]<75 && arr[i]>=60)
+        {
+            printf("\nGrade B");
+        }
+
+        else if(arr[i]<60 && arr[i]>=40)
+        {
+            printf("\nGrade C");
+        }
+
+        else if(arr[i]<40)
+        {
+            printf("\nGrade D");
+        }
+    }
+}
